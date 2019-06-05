28aa46cc94e8c9a886c6f5a5f68455f01a08eec5
Matjaž Mav <matjaz.mav@gmail.com>
2019-06-05 12:38:44 +0200

    Updated install & readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # i3wm configuretion for Ubuntu 18.04 LTS
 
-1. Clone this repository into ~/.config/i3
+## Requirements
+1. git
+2. git lfs
+
+## Install
+1. Clone this repository into `~/.config/i3`
+```
+$ git clone git@github.com:matjazmav/i3wm-config.git ~/.config/i3
+```
+
 2. Run install.sh script to install all required dependencies
+```
+$ ~/.config/i3/install.sh
+```
+
 3. Restart computer and enyoj i3 on Ubuntu 18.04 LTS.
+
+## Configure
+TODO
--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-apt update
+username=$USER
+if [ "$username" == "root" ]
+then
+	echo "Run this script without sudo..."
+	exit 1
+fi
 
-apt install -y \
+sudo apt update
+sudo apt install -y \
+	i3
 	python3 \
 	python3-pip \
 	py3status \
@@ -11,18 +18,27 @@ apt install -y \
 	flameshot \
 	redshift \
 	scrot
-
-pip3 install \
+sudo pip3 install \
 	git+https://github.com/enkore/i3pystatus.git \
 	i3ipc
 
 # Brightness
-wget -O light_amd64.deb https://github.com/haikarainen/light/releases/download/v1.2/light_1.2_amd64.deb
-dpkg -i light_amd64.deb
-rm light_amd64.deb
+sudo wget -O light_amd64.deb https://github.com/haikarainen/light/releases/download/v1.2/light_1.2_amd64.deb
+sudo dpkg -i light_amd64.deb
+sudo rm light_amd64.deb
+sudo light -N 11
+cat << EOF | sudo tee -a /etc/sudoers
 
-usermod -a -G video $USER
+# i3wm config for $username
+$username ALL=(ALL:ALL) NOPASSWD: /usr/bin/light
+EOF
 
-light -N 11
+# Fonts
+# mkdir -p ~/.fonts
+# wget https://use.fontawesome.com/releases/v5.8.2/fontawesome-free-5.8.2-desktop.zip
+# unzip fontawesome-free-5.8.2-desktop.zip
+# rm fontawesome-free-5.8.2-desktop.zip
+# cp fontawesome-free-5.8.2-desktop/otfs/*.otf ~/.fonts
+# rm -rf fontawesome-free-5.8.2-desktop/
 
 echo "Done. Restart is required."
